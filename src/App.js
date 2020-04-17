4ff987648fdf0cf42f2550d714eb937b3d2bb9c1
Erica Wang <ericawang@Ericas-MacBook-Pro.local>
2020-04-17 15:26:11 -0400

    basically finished just update doesnt work rn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiUrl = "http://localhost:5000"
+
+class App extends Component{
+  positions = [{position: "absolute", size: "8", left: '829', top: '441'}];
+
+  constructor(props){
+    super(props);
+    let position = [];
+    axios.get(apiUrl+'/locations').then((response)=> {
+      for (const person of response.data){
+        var car = {id:person.id, x:person.x, y:person.y};
+        position.push(car);
+        this.positions = position;
+      }
+    });
+  }
+
+  updateLocation(){
+    //this.setState({position: this.state.text+1});
+    let position = [];
+    axios.get(apiUrl+'/locations').then((response)=> {
+      for (const person of response.data){
+        var car = {id:person.id, x:person.x, y:person.y};
+        position.push(car);
+        this.positions = position;
+      }
+    });
+    this.render();
+  }
+
+  componentDidMount(){
+    const interval = setInterval(() => {
+      this.updateLocation();
+    }, 1000);
+  }
+
+  render() {
+    
+    const items = this.positions.map(function(item){
+      return {position: "absolute",size: "8",left: item['x'],top: item['y']}
+    });/*
+    console.log(items);
+      return (
+        <div>
+          {items.map((item)=><p style={{position: "absolute",size: "8",left: item['x'],top: item['y']}}>aa</p>)}
+        </div>
+      )
+    */
+    const str = [{position: "absolute", size: "8", left: 829, top: 441},
+    {position: "absolute", size: "8", left: 365, top: 216},
+    {position: "absolute", size: "8", left: 216, top: 484},
+    {position: "absolute", size: "8", left: 386, top: 64},
+    {position: "absolute", size: "8", left: 829, top: 441},
+    {position: "absolute", size: "8", left: 365, top: 216},
+    {position: "absolute", size: "8", left: 216, top: 484},
+    {position: "absolute", size: "8", left: 386, top: 64},
+    {position: "absolute", size: "8", left: 829, top: 441}];
+    
+    return(
+      <div>
+        <p style={str[0]}>str</p>
+        <div>
+          {items.map((item)=><p style={{position: "absolute",size: "8",left: item['x'],top: item['y']}}>aa</p>)}
+        </div>
+        <div>
+          {items.map((item)=>console.log(item))}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
